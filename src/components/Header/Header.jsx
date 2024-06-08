aaea2cd55cba68015ce1e6780a7ef1d5c6cb7bdb
RockTINTIN21 <sashalexjr@gmail.com>
2024-06-08 23:14:41 +0500

    Добавлен css модуль для Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import './Header.css';
+import styles from './Header.module.css';
 function Header() {
     return (
-        <img className='logo' src="/logo.svg" alt="Логотип журанала"/>
+        <img className={styles.logo} src="/logo.svg" alt="Логотип журанала"/>
     );
 }
 
